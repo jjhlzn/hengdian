43d4536f8bf9dbf72274191aed135ff6652b7189
jjhlzn <jinjunhang@hotmail.com>
2014-05-19 23:20:42 +0800

    开发

--- hengdian/settings.py
+++ hengdian/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'lottery',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -85,3 +86,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
--- hengdian/urls.py
+++ hengdian/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hengdian.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^lottery/', include('lottery.urls')),
     url(r'^admin/', include(admin.site.urls)),
+	
 )
