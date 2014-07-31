966f53b0e38a52c38697680d92177aa329ae7e70
jjhlzn <jinjunhang@hotmail.com>
2014-07-31 13:56:52 +0800

    日志系统前端

--- logsystem/views.py
+++ logsystem/views.py
@@ -4,41 +4,46 @@ from logsystem.models import *
 import datetime
 from django.db import connection
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 CounterPerPage = 100
 
 def index(request):
+	# parse query parameters
 	qs = parse_qs(request.META['QUERY_STRING']) 
-	day =  qs.get('date',[datetime.datetime.now().strftime("%Y-%m-%d")])[0] 
-	page_no = int(qs.get('page',[1])[0])
-	start_time = qs.get('start_time',[''])[0]
-	end_time = qs.get('end_time',[''])[0]
-	search_content = unicode(qs.get('search_content',[''])[0], 'UTF-8')
-	time_from_now = qs.get('time_from_now',[''])[0]
+	day =  GetQueryParam(qs, 'date', 
+				datetime.datetime.now().strftime("%Y-%m-%d")) 
+	page_no = int(GetQueryParam(qs, 'page', 1))
+	start_time = GetQueryParam(qs, 'start_time', '')
+	end_time = GetQueryParam(qs, 'end_time', '')
+	search_content = unicode(GetQueryParam(qs, 'search_content', ''), 'UTF-8')
+	thread = GetQueryParam(qs, 'thread', '')
+	time_from_now = GetQueryParam(qs, 'time_from_now', '')
 	if (IsNotNullOrEmpty(time_from_now)):
-		start_time = (datetime.datetime.now() + 
-							datetime.timedelta(seconds=-int(time_from_now))).strftime("%H:%M:%S")
+		start_time = (datetime.datetime.now() + datetime.timedelta(seconds=-int(time_from_now))).strftime("%H:%M:%S")
 	
-	records = OrderSystemLogRecord.objects.all();
+	# search log record according to query parameters
+	query = OrderSystemLogRecord.objects.all()
 	
 	if IsNotNullOrEmpty(start_time):
 		the_time = day + " " + start_time
 	else:
 		the_time = day
-	print the_time+"\n"
-	records = records.filter(time__gte=the_time)
+	query = query.filter(time__gte=the_time)
 	
 	if IsNotNullOrEmpty(end_time):
 		the_time = day + " " + end_time + ",999"
-		print the_time+"\n"
-		records = records.filter(time__lte=the_time)
+		query = query.filter(time__lte=the_time)
 	
+	if IsNotNullOrEmpty(thread):
+		query = query.filter(thread=thread)
 	
 	if IsNotNullOrEmpty(search_content):
-		print "search_content=["+search_content+"]\n" 
-		records = records.filter(content__icontains=search_content) 
-	records = records.order_by("time")
-	p = Paginator(records, CounterPerPage)
+		query = query.filter(Q(content__icontains=search_content) 
+									| Q(clazz__icontains=search_content)) 
+	query = query.order_by("time")
+	
+	p = Paginator(query, CounterPerPage)
 	page = p.page(page_no)
 	
 	return render(request, 
@@ -48,8 +53,12 @@ def index(request):
 				   'date': day,
 				   'start_time': start_time,
 				   'end_time': end_time,
-				   'search_content': search_content});
+				   'search_content': search_content,
+				   'thread': thread},);
 
 def IsNotNullOrEmpty(value):
     return value is not None and len(value) > 0
+ 
+def GetQueryParam(qs, name, default):
+	return qs.get(name,[default])[0]
 
