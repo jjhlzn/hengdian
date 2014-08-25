8b3982d4d367823e822dda5900bb79f7ae0fe538
jjhlzn <jinjunhang@hotmail.com>
2014-08-25 17:00:01 +0800

    更新

--- order/views/views_report.py
+++ order/views/views_report.py
@@ -1,17 +1,16 @@
 # coding:UTF-8
 
 from django.shortcuts import render
+from urlparse import urlparse, parse_qs
 import _mssql
 
 server = '127.0.0.1'
 user = 'sa'
 password = '123456'
 
-
 def get_connection():
     conn = _mssql.connect(server=server, user=user, password=password, database='hdbusiness', charset="utf8")
     return conn
-
 
 def get_rows_from_orders(sql, parameter=[]):
     data = []
@@ -22,13 +21,16 @@ def get_rows_from_orders(sql, parameter=[]):
     conn.close()
     return data
 
-
 def order_statistic(request):
-    sql = "select * from report.dbo.t_ordersystem_dailyorder where order_date >= '2014-4-18' order by order_date"
-    rows = get_rows_from_orders(sql)
-    #top 10 comedate
-    sql = "select top 10 * from report.dbo.t_ordersystem_dailyorder where order_date >= '2014-4-18' order by success_order_count desc"
-    top10_days = get_rows_from_orders(sql)
-    return render(request, 'order/order_statistic.html', {"data": rows, "top10_days": top10_days})
+    qs = parse_qs(request.META['QUERY_STRING'])
 
+    sql = "select * from report.dbo.t_ordersystem_dailyorder where order_date >= '2013-1-1' and order_date <= '2013-12-31' order by order_date"
+    dataset_2013 = get_rows_from_orders(sql)
+    sql = "select * from report.dbo.t_ordersystem_dailyorder where order_date >= '2014-1-1' and order_date <= '2014-12-31' order by order_date"
+    dataset_2014 = get_rows_from_orders(sql)
+
+    return render(request, 'order/order_statistic.html', {"data0": dataset_2014, "data1": dataset_2013})
+
+def get_query_param(qs, name, default):
+    return qs.get(name,[default])[0]
 
